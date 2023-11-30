eaa8f6deda0e69c9fb3d890baae9b8895e0585cf
Yakuts Volodymyr <eagle9995.an@gmail.com>
2023-11-30 07:44:29 +0100

    perf(frontend): error handling

--- task-manager-client/src/App.tsx
+++ task-manager-client/src/App.tsx
@@ -1,13 +1,38 @@
 import "./App.css";
 import { MainPage } from "./components/mainpage";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import {
+  QueryClient,
+  QueryErrorResetBoundary,
+  QueryClientProvider,
+} from "@tanstack/react-query";
+import { ErrorBoundary } from "react-error-boundary";
 
 function App() {
   const queryClient = new QueryClient();
   return (
     <>
       <QueryClientProvider client={queryClient}>
-        <MainPage />
+        <QueryErrorResetBoundary>
+          {({ reset }) => (
+            <ErrorBoundary
+              onReset={reset}
+              fallbackRender={({ resetErrorBoundary }) => (
+                <div>
+                  There was an error!
+                  <button
+                    onClick={() => {
+                      resetErrorBoundary();
+                    }}
+                  >
+                    Try again
+                  </button>
+                </div>
+              )}
+            >
+              <MainPage />
+            </ErrorBoundary>
+          )}
+        </QueryErrorResetBoundary>
       </QueryClientProvider>
     </>
   );
