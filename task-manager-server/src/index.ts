728c7645855aaf07922cb2dc083b0ef52a6f38dc
Yakuts Volodymyr <eagle9995.an@gmail.com>
2023-11-30 07:50:01 +0100

    fix(backend): solve cors error

--- task-manager-server/src/index.ts
+++ task-manager-server/src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import cors from 'cors';
 import {connectDB} from './config';
 import {router} from './routes';
 
@@ -8,10 +9,23 @@ const PORT = process.env.PORT || 3001;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
 
 connectDB();
 
 app.use('/api/tasks', router);
+
+app.use(
+  (
+    err: any,
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction,
+  ) => {
+    console.error(err.stack);
+    res.status(500).send("Something went wrong!");
+  },
+);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
