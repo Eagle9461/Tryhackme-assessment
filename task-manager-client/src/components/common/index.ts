9fb19584796959fe278c63f5740a71223b081469
Yakuts Volodymyr <eagle9995.an@gmail.com>
2023-12-01 03:27:06 +0100

    feat(frontend): implement Spinner

--- task-manager-client/src/components/common/index.ts
+++ task-manager-client/src/components/common/index.ts
@@ -2,3 +2,4 @@ export { default as BaseInputField } from './BaseInputField';
 export { default as BaseTextarea } from './BaseTextarea';
 export { default as BaseToogle } from './BaseToogle';
 export { default as TrushbinIcon } from './TrushbinIcon';
+export { default as BaseSpinner} from './BaseSpinner';
--- task-manager-client/src/components/mainpage/MainPage.tsx
+++ task-manager-client/src/components/mainpage/MainPage.tsx
@@ -1,12 +1,19 @@
 import { Suspense } from "react";
 import { MainHeader } from ".";
 import { ConfirmModal, TaskList, TaskModal } from "..";
+import BaseSpiner from "../common/BaseSpinner";
 
 const MainPage: React.FC = () => {
   return (
     <div className="container">
       <MainHeader />
-      <Suspense fallback={<div>Loading</div>}>
+      <Suspense
+        fallback={
+          <div>
+            <BaseSpiner />
+          </div>
+        }
+      >
         <TaskList />
       </Suspense>
       <TaskModal />
