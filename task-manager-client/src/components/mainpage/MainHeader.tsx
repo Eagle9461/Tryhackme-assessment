49d303f80509c733ca7a12e4ef7e1e0690231f65
Yakuts Volodymyr <eagle9995.an@gmail.com>
2023-12-01 03:46:30 +0100

    perf(frontend): add heading

--- task-manager-client/src/components/mainpage/MainHeader.tsx
+++ task-manager-client/src/components/mainpage/MainHeader.tsx
@@ -4,20 +4,25 @@ import { TaskModalStatus } from "../../types";
 const MainHeader: React.FC = () => {
   const dispatch = useAppDispatch();
   return (
-    <div className="flex justify-end mb-4">
-      <button
-        onClick={() =>
-          dispatch(
-            changeModalStatus({
-              modalStatus: TaskModalStatus.OPEN,
-              currentId: undefined,
-            })
-          )
-        }
-        className="bg-gray-50 dark:bg-gray-700 dark:text-gray-400 hover:bg-gray-500 text-white font-bold py-2 px-4 rounded-full"
-      >
-        Create New Task
-      </button>
+    <div>
+      <h1 className="my-8 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl">
+        Task Management
+      </h1>
+      <div className="flex justify-end mb-4">
+        <button
+          onClick={() =>
+            dispatch(
+              changeModalStatus({
+                modalStatus: TaskModalStatus.OPEN,
+                currentId: undefined,
+              })
+            )
+          }
+          className="bg-gray-50 dark:bg-gray-700 dark:text-gray-400 hover:bg-gray-500 text-white font-bold py-2 px-4 rounded-full"
+        >
+          Create New Task
+        </button>
+      </div>
     </div>
   );
 };
